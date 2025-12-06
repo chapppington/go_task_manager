94197723dab29826d6928e054384aba13016097e
chapppington <stas.mega.96@bk.ru>
2025-12-06 21:43:30 +0700

    (chore) refactored main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"crud/config"
@@ -58,34 +54,9 @@ func main() {
 		port = 8000
 	}
 
-	// Создаем HTTP сервер
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", port),
-		Handler: r,
+	// Запускаем сервер
+	log.Printf("Server starting on port %d", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), r); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
 	}
-
-	// Запускаем сервер в горутине
-	go func() {
-		log.Printf("Server starting on port %d", port)
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Server failed to start: %v", err)
-		}
-	}()
-
-	// Ожидаем сигнал для graceful shutdown
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-
-	log.Println("Shutting down server...")
-
-	// Graceful shutdown
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v", err)
-	}
-
-	log.Println("Server exited")
 }
